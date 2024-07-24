01ac79ddbb399ba4f0f5d893e02835f8ed49f710
dimanranawaka <dimanranawaka@gmail.com>
2024-07-24 14:09:34 +0530

    create movies data service

--- App.jsx
+++ App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import axios from 'axios';
+
+const getPopularMovies = async () => {
+  const response = await axios.get(
+    'https://api.themoviedb.org/3/movie/550?api_key=2a2d4105c6f076353ee5274f587aa247',
+  );
+  console.log(JSON.stringify(response.data.results[0], null, 2));
+};
 
 const App = () => {
+  getPopularMovies();
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello, Diman Ranawaka!</Text>
