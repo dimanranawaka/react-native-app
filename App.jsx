8658058c74da3297dc8e686af089a64bbb0c52e4
dimanranawaka <dimanranawaka@gmail.com>
2024-07-25 00:05:01 +0530

    implement a stack navigator

--- App.jsx
+++ App.jsx
@@ -1,18 +1,18 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {View} from 'react-native';
 import Home from './screens/Home';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Home></Home>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
