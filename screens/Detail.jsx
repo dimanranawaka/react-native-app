3eafa5e231d64f4d5971f4faa5155107c5fa8637
dimanranawaka <dimanranawaka@gmail.com>
2024-08-01 23:52:22 +0530

    implement movie detail component

--- screens/Detail.jsx
+++ screens/Detail.jsx
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import {Text} from 'react-native';
+import {ScrollView, Image, StyleSheet} from 'react-native';
 import {getMovie} from '../services/services';
-import {ScrollView} from 'react-native-gesture-handler';
+// import {ScrollView} from 'react-native-gesture-handler';
+
+const placeholderImage = require('../assets/images/placeholder.png');
 
 const Detail = ({route, navigation}) => {
   const movieId = route.params.movieId;
@@ -20,11 +22,31 @@ const Detail = ({route, navigation}) => {
     <React.Fragment>
       {loaded && (
         <ScrollView>
-          <Text>{movieDetail.title}</Text>
+          <Image
+            resizeMode="cover"
+            style={styles.image}
+            source={
+              movieDetail.poster_path
+                ? {
+                    uri:
+                      'https://image.tmdb.org/t/p/w500' +
+                      movieDetail.poster_path,
+                  }
+                : placeholderImage
+            }
+          />
         </ScrollView>
       )}
     </React.Fragment>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 120,
+    borderRadius: 20,
+  },
+});
 
 export default Detail;
