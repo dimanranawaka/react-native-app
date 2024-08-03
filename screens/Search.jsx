b2f630e908bbda4daf29b7ed14312435bf323089
dimanranawaka <dimanranawaka@gmail.com>
2024-08-03 10:07:43 +0530

    implement search component

--- screens/Search.jsx
+++ screens/Search.jsx
@@ -1,36 +1,84 @@
-import React ,{useState} from 'react';
-import { SafeAreaView, View, TextInput, StyleSheet, TouchableOpacity } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
-import {searchMovieTv} from "../services/services";
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  View,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Text,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {searchMovieTv} from '../services/services';
+import Card from '../components/Card';
+import Error from '../components/Error';
 
 const Search = ({navigation}) => {
   const [text, onChangeText] = useState();
   const [searchResults, setSearchResults] = useState();
+  const [error, setError] = useState(false);
 
-  const onsubmit = query => {
-    searchMovieTv(query,'movie').then(data => {
-      // setSearchResults(data.results);
-      console.log(data);
-    });
+  const onSubmit = query => {
+    Promise.all([searchMovieTv(query, 'movie'), searchMovieTv(query, 'tv')])
+      .then(([movies, tv]) => {
+        const data = [...movies, ...tv];
+        setSearchResults(data);
+      })
+      .catch(() => {
+        setError(true);
+      });
   };
+
   return (
     <React.Fragment>
       <SafeAreaView>
         <View style={styles.container}>
           <View style={styles.form}>
-          <TextInput
-            style={styles.input}
-            placeholder={'Search Movie or TV Show'}
-            onChangeText={onChangeText}
-            value={text}
-          />
+            <TextInput
+              style={styles.input}
+              placeholder={'Search Movie or TV Show'}
+              onChangeText={onChangeText}
+              value={text}
+            />
           </View>
           <TouchableOpacity
             onPress={() => {
-             onsubmit(text)
+              onSubmit(text);
             }}>
             <Icon name={'search-outline'} size={30} />
           </TouchableOpacity>
+        </View>
+
+        <View style={styles.searchItems}>
+          {/* Searched items results */}
+          {searchResults && searchResults.length > 0 && (
+            <FlatList
+              numColumns={3}
+              data={searchResults}
+              renderItem={({item}) => (
+                <Card navigation={navigation} item={item} />
+              )}
+              keyExtractor={item => item.id}
+            />
+          )}
+
+          {/* When searched but no results */}
+          {searchResults && searchResults.length == 0 && (
+            <View style={styles.noResults}>
+              <Text>No results matching your criteria.</Text>
+              <Text>Try different keywords.</Text>
+            </View>
+          )}
+
+          {/* When nothing is searched */}
+          {!searchResults && (
+            <View style={styles.empty}>
+              <Text>Type something to start searching</Text>
+            </View>
+          )}
+
+          {/* Error */}
+          {error && <Error />}
         </View>
       </SafeAreaView>
     </React.Fragment>
