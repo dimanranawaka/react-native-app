271a44f78beafd37416bb97df7266427c4981df8
dimanranawaka <dimanranawaka@gmail.com>
2024-08-03 08:49:44 +0530

    implement search component

--- screens/Search.jsx
+++ screens/Search.jsx
@@ -1,18 +1,30 @@
 import React ,{useState} from 'react';
-import {SafeAreaView, View, TextInput,StyleSheet} from 'react-native';
+import { SafeAreaView, View, TextInput, StyleSheet, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const Search = ({navigation}) => {
   const [text, onChangeText] = useState();
+  const onsubmit = query => {
+    console.log(query);
+  };
   return (
     <React.Fragment>
-      <SafeAreaView style={styles.container}>
-        <View>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <View style={styles.form}>
           <TextInput
             style={styles.input}
             placeholder={'Search Movie or TV Show'}
             onChangeText={onChangeText}
             value={text}
           />
+          </View>
+          <TouchableOpacity
+            onPress={() => {
+             onsubmit(text)
+            }}>
+            <Icon name={'search-outline'} size={30} />
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     </React.Fragment>
@@ -31,6 +43,19 @@ const styles = StyleSheet.create({
     paddingTop: 60,
     flexDirection: 'row',
     alignItems: 'center',
+  },
+  form: {
+    flexBasis: 'auto',
+    flexGrow: 1,
+    paddingRight: 8,
+  },
+
+  searchItems: {
+    padding: 5,
+  },
+
+  noResults: {
+    paddingTop: 20,
   },
 });
 
